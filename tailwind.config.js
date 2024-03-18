f815a6401fa38882c523ebf73edf8cd2ff619e1e
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-18 11:39:15 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,24 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      sm: "16px",
+      md: "20px",
+      lg: "24px",
+      xl: "38px",
+      xxl: "48px",
+      "2xl": "68px",
+      "3xl": "86px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "rich-black": "#b2b2b2",
+        "dark-Blblackack": "#1e1e1e",
+        gray: "#656463",
+        "dark-gray": "#b9b9b9",
+        "light-white": "#f6f6f6",
+        "light-gray": "#eceae7",
+        bombay: "#b6b6b6",
       },
     },
   },
