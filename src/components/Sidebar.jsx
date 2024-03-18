3eddbf5cfc023f3971d646b2190a5259126bd405
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-18 12:18:37 +0530

    push my

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,86 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { RxCross2 } from "react-icons/rx";
+import { sidebarLink } from "./common/Helper";
+import { Logo } from "./common/Icon";
+const Sidebar = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+    document.body.style.overflow = "auto";
+  };
+
+  return (
+    <div className="bg-mirage">
+      <div className="container pt-7 max-w-[1152px] mx-auto px-3">
+        <div className="flex justify-between items-start">
+          <Logo />
+          <div className="relative flex">
+            {/* Overlay */}
+            {isSidebarOpen && (
+              <div
+                className="fixed inset-0 bg-black opacity-50"
+                onClick={closeSidebar}
+              ></div>
+            )}
+
+            {/* Sidebar */}
+            <div
+              className={`sidebar bg-white end-0 fixed top-0 w-full max-w-[300px] h-full transition-transform transform z-50 ${
+                isSidebarOpen ? "translate-x-0" : "-translate-x-[-100%]"
+              }`}
+            >
+              <div className="min-h-screen flex flex-col justify-center items-center">
+                {/* Your sidebar content goes here */}
+                <button
+                  onClick={closeSidebar}
+                  className="absolute top-3 left-3 text-tia-maria"
+                >
+                  <RxCross2 className="w-[43px] h-[43px]" />
+                </button>
+                <ul className="flex flex-col gap-5">
+                  {sidebarLink.map((value, index) => {
+                    return (
+                      <li
+                        key={index}
+                        onClick={closeSidebar}
+                        className="text-center"
+                      >
+                        <Link
+                          aria-label={value.title}
+                          className="text-mirage font-Inter font-medium text-xl opacity-75 hover:opacity-100 transition-all ease-in-out duration-300"
+                          key={index}
+                          href={value.path}
+                        >
+                          {value.title}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+
+            {/* Main content */}
+            <div className="flex-1">{children}</div>
+
+            {/* Toggle button */}
+            <button className=" text-tia-maria" onClick={openSidebar}>
+              <AiOutlineMenu className="w-[43px] h-[43px]" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,7 @@
+export const sidebarLink = [
+  { title: "HOME", path: "#HOME" },
+  { title: "ABOUT US", path: "#ABOUT-US" },
+  { title: "ATTORNEYS ", path: "#ATTORNEYS" },
+  { title: "RESULTS", path: "#RESULTS" },
+  { title: "MEDIA", path: "#MEDIA" },
+];
