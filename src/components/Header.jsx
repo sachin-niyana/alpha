cb58d1ca83f2d0af84f6c26c799ed18b94193400
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-19 09:28:44 +0530

    link update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,26 +34,26 @@ const Header = () => {
           height={604}
           src="/assets/images/header/alpha.png"
         />
-        <div className="flex justify-between border-e-[1px] border-rich-black">
-          <div className="m-0">
-            <div className="bg-rich-black h-[43px] w-[1px] xl:ms-[13px]"></div>
-            <p className="font-inter font-normal text-md text-rich-black pt-[26px] tracking-widest uppercase">
-              B2B Sales Agency
-            </p>
-            <h1 className="font-inter font-normal text-3xl text-rich-black uppercase leading-[120%] max-w-[550px] pt-[9px]">
-              Corporate marketing agency{" "}
-            </h1>
-            <div className="flex gap-2.5 items-center mt-10 group max-w-[140px]">
-              <p className="font-inter font-thin text-md text-rich-black">
-                Our work
-              </p>
-              <div className="group-hover:translate-x-2 transition-all ease-in-out duration-300">
-                <RightArrow />
+        <div className="flex lg:flex-row flex-col items-center border-e-[1px] border-rich-black">
+          <div className="flex flex-col w-full border-e-[1px] border-rich-black">
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <div className="bg-rich-black h-[43px] w-[1px] xl:ms-[13px]"></div>
+                <p className="font-inter font-normal text-md text-rich-black pt-[26px] tracking-widest uppercase">
+                  B2B Sales Agency
+                </p>
+                <h1 className="font-inter font-normal text-3xl text-rich-black uppercase leading-[120%] max-w-[550px] pt-[9px]">
+                  Corporate marketing agency{" "}
+                </h1>
+                <div className="flex gap-2.5 items-center mt-10 group max-w-[140px]">
+                  <p className="font-inter font-thin text-md text-rich-black">
+                    Our work
+                  </p>
+                  <div className="group-hover:translate-x-2 transition-all ease-in-out duration-300">
+                    <RightArrow />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="flex">
-            <div className="border-e-[1px] border-rich-black h-full">
               <Image
                 className="mt-[185px]"
                 width={180}
@@ -61,36 +61,44 @@ const Header = () => {
                 src="/assets/images/header/study.png"
               />
             </div>
-            <div className="m-0">
-              <div className="absolute top-[8%] right-[4%] w-[37px] h-[74px] bg-linear-gradient-btn rounded-tr-full rounded-br-full rotate-[-30deg]"></div>
-              <div className="flex flex-col w-[153px] mt-[230px]">
-                <div className="border-b-[1px] border-rich-black pb-[33px] pt-[43px]">
-                  <h3 className="text-center font-inter font-normal text-xxl text-rich-black leading-[120%]">
-                    {" "}
-                    {counters.counter1}+
-                  </h3>
-                  <p className="font-inter font-normal text-sm text-gray max-w-[80px] text-center mx-auto">
-                    Years of experience
-                  </p>
-                </div>
-                <div className="border-b-[1px] border-rich-black pb-[33px] pt-[43px]">
-                  <h3 className="text-center font-inter font-normal text-xxl text-rich-black leading-[120%]">
-                    {" "}
-                    {counters.counter2}+
-                  </h3>
-                  <p className="font-inter font-normal text-sm text-gray max-w-[80px] text-center mx-auto">
-                    Years of experience
-                  </p>
-                </div>
-                <div className="border-b-[1px] border-rich-black pb-[33px] pt-[43px]">
-                  <h3 className="text-center font-inter font-normal text-xxl text-rich-black leading-[120%]">
-                    {" "}
-                    {counters.counter3}+
-                  </h3>
-                  <p className="font-inter font-normal text-sm text-gray max-w-[80px] text-center mx-auto">
-                    Years of experience
-                  </p>
-                </div>
+            <div className="relative h-[439px]">
+              <Image
+                className="absolute min-w-[1087px] right-0 w-full"
+                width={1087}
+                height={439}
+                src="/assets/images/header/screen.png"
+              />
+            </div>
+          </div>
+          <div className="w-[153px]">
+            <div className="absolute top-[4%] right-[4%] w-[37px] h-[74px] bg-linear-gradient-btn rounded-tr-full rounded-br-full rotate-[-30deg]"></div>
+            <div className="flex lg:flex-col flex-row justify-center gap-20">
+              <div className="border-b-[1px] border-rich-black pb-[33px] pt-[43px]">
+                <h3 className="text-center font-inter font-normal text-xxl text-rich-black leading-[120%]">
+                  {" "}
+                  {counters.counter1}+
+                </h3>
+                <p className="font-inter font-normal text-sm text-gray max-w-[80px] text-center mx-auto">
+                  Years Of Experience
+                </p>
+              </div>
+              <div className="border-b-[1px] border-rich-black pb-[33px] pt-[43px]">
+                <h3 className="text-center font-inter font-normal text-xxl text-rich-black leading-[120%]">
+                  {" "}
+                  {counters.counter2}+
+                </h3>
+                <p className="font-inter font-normal text-sm text-gray max-w-[80px] text-center mx-auto">
+                  Years Of Experience
+                </p>
+              </div>
+              <div className="border-b-[1px] border-rich-black pb-[33px] pt-[43px]">
+                <h3 className="text-center font-inter font-normal text-xxl text-rich-black leading-[120%]">
+                  {" "}
+                  {counters.counter3}+
+                </h3>
+                <p className="font-inter font-normal text-sm text-gray max-w-[80px] text-center mx-auto">
+                  Years Of Experience
+                </p>
               </div>
             </div>
           </div>
