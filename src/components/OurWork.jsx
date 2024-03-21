ebcdf76a801ec6bc0a01293d8b210f562fc73bc6
kulvinder02 <kulvinderjangra02@gmail.com>
2024-03-21 10:03:12 +0530

    done

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Image from "next/image";
-import { HalfGradientCircle } from "./common/Icon";
+import { CirclesVector, DoubleTringalVector, HalfGradientCircle } from "./common/Icon";
 
 const OurWork = () => {
   return (
-    <div>
+    <div className="relative">
+      <div className="absolute bottom-[45px] left-[35px] hidden md:block"> <DoubleTringalVector /> </div>
+      <span className="absolute top-[10%] right-[6%] hidden md:block"> <CirclesVector /> </span>
       <div className="container max-w-[1152px] mx-auto px-3">
-        <div className="h-[229px] w-[1px]"></div>
+        <div className=" h-[60px] xl:h-[229.3px] w-[1px] bg-[#2B2B2B] absolute top-0 left-[7%] xl:left-[14%] hidden md:block"></div>
         <div className="sm:border-r-[1px] lg:mr-9 lg:items-start border-rich-black">
-          <div className="flex flex-wrap justify-center">
+          <div className="flex flex-wrap justify-center pt-[60px] lg:pt-[100px] xl:pt-[244px]">
             <div className="lg:w-[70%] xl:w-[60%]">
               <div className="flex items-center">
                 <span>
