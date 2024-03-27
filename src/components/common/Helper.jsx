f9727b080c107c35cba3ab0c93a6b86bb01335db
sachin-niyana <sachinjakhar100@gmail.com>
2024-03-27 14:41:15 +0530

    done

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -5,17 +5,3 @@ export const sidebarLink = [
   { title: "LATEST", path: "#latest" },
   { title: "CONTECT", path: "#contect" },
 ];
-
-export const sliderData = [
-  {
-    title: "ENGIE IMPACT",
-    growth: "Growth in Pipeline",
-    sqls: "SQLs",
-    roi: "ROI",
-    image: "/assets/images/engie-impact/webp/wpp.webp",
-    heading: "Business Development (Director)",
-    heading: "Sustainable Resource Management (SRM)",
-    discripstion:
-      "“Alpha² presented to us the exact solution required, agile project delivery and complete transparency throughout each reporting review. Given the volatile energy market, we wanted to understand the specific challenges our prospective clients were facing to help us find ways in which to support them. We worked closely with Alpha² right from the start and achieved great results that have helped shape our sales strategy moving forwards.”",
-  },
-];
